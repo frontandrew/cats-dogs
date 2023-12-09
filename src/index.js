9972da4ae431a34e9b2079a166bf4234b96235f6
AndrewBubnov <jackblack.nt@gmail.com>
2023-12-09 19:16:45 +0500

    feat: rank and res sorting

--- src/index.js
+++ src/index.js
@@ -1,6 +1,39 @@
 import { data } from "./cats_and_dogs.js";
 
-// Решение ->
+// Решение -> 
+
+const TOP_QUANTITY = 5
+
+function compareByRank (a, b) {
+  if (a.likes > b.likes) return -1
+  if (a.likes < b.likes) return 1
+  return 0
+}
+
+function compareByRes (a, b) {
+  const aRes = a.width * a.height
+  const bRes = b.width * b.height
+
+  if (aRes > bRes) return -1
+  if (aRes < bRes) return 1
+  return 0
+}
+
+// Собираем массив и обогащаем id каждую запись
+const allPets = Object.values(data).map((value, index) => ({ id: index + 1, ...value }))
+
+// Ранжируем по колличесиву лайков 
+const rankedPets = allPets.sort((a, b) => compareByRank(a, b))
+
+const top = rankedPets.slice(0, TOP_QUANTITY).sort((a ,b) => compareByRes(a, b))
+const rest = rankedPets.slice(TOP_QUANTITY).sort((a ,b) => compareByRes(a, b))
+
+const result = { top, rest }
+
+console.log('RESULT: ', result) 
+
+// <-
+
 
 // Проверки
 result.top.length !== 5 && console.error("top: Должно быть 5 зверюшек");
