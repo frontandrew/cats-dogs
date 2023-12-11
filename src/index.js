dcb70e2cb6d315218f6e9ce50e65e740d4acf0ca
AndrewBubnov <jackblack.nt@gmail.com>
2023-12-11 12:07:34 +0500

    feat(3): format

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import { data } from "./cats_and_dogs.js";
 
 const catsAndDogs = Object.entries(data)
-    .reduce((acc, [id, value]) => [...acc, { ...value, id }], [])
-    .sort((a1, a2) => (a1.likes > a2.likes ? -1 : 1));
+  .reduce((acc, [id, value]) => [...acc, { ...value, id }], [])
+  .sort((a1, a2) => (a1.likes > a2.likes ? -1 : 1));
 
 const result = {
-    top: catsAndDogs.slice(0, 5).sort((a1, a2) => (a1.width * a1.height > a2.height * a2.width ? -1 : 1)),
-    rest: catsAndDogs.slice(5).sort((a1, a2) => (a1.width * a1.height > a2.height * a2.width ? -1 : 1)),
+  top: catsAndDogs
+    .slice(0, 5)
+    .sort((a1, a2) => (a1.width * a1.height > a2.height * a2.width ? -1 : 1)),
+  rest: catsAndDogs
+    .slice(5)
+    .sort((a1, a2) => (a1.width * a1.height > a2.height * a2.width ? -1 : 1)),
 };
 
 console.log(result);
