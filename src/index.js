8a71a082c83b3bdf737a51a2b39f3371d7c12302
AndrewBubnov <jackblack.nt@gmail.com>
2023-12-11 11:19:31 +0500

    feat(2): separate recognize calc to async func

--- src/index.js
+++ src/index.js
@@ -10,15 +10,28 @@ const getCats = async () => {
   // из-за чего в cats попадают все pets?
   // как использовать async await, чтобы до возвращения результата
   // getCats() дождаться выполнения cats
-  
-  const recognizedCats = async (animals) =>
-    animals.filter(async (animal) => {
-      const res = await magicalCatRecognizer.recognize(animal);
 
-      console.log(`fitler(ID: ${animal.id}):`, res);
-      return res;
-    });
-  const cats = await recognizedCats(pets);
+  // const recognizedCats = async (animals) =>
+  //   animals.filter(async (animal) => {
+  //     const res = await magicalCatRecognizer.recognize(animal);
+
+  //     console.log(`fitler(ID: ${animal.id}):`, res);
+  //     return res;
+  //   });
+
+  // const cats = await recognizedCats(pets);
+
+  // решение 2
+
+  const TOP_QUANTITY = 3;
+
+  const recognizedPets = await Promise.all(
+    pets.map((pet) => magicalCatRecognizer.recognize(pet))
+  );
+
+  const cats = pets
+    .filter((pet, index) => recognizedPets[index])
+    .slice(0, TOP_QUANTITY);
 
   console.log(`setCats(res):`, { cats, pets });
   return cats;
