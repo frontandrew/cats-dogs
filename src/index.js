1505a52519cc70b435d2c377d36d0cb770ace68e
AndrewBubnov <jackblack.nt@gmail.com>
2023-12-11 11:45:54 +0500

    feat(3): init commit

--- src/index.js
+++ src/index.js
@@ -1,37 +1,21 @@
-import { catAndDogFetcher, magicalCatRecognizer } from "./lib.js";
+import { data } from "./cats_and_dogs.js";
 
-const getCats = async () => {
-  // Решение ===
+const catsAndDogs = Object.entries(data)
+    .reduce((acc, [id, value]) => [...acc, { ...value, id }], [])
+    .sort((a1, a2) => (a1.likes > a2.likes ? -1 : 1));
 
-  const TOP_QUANTITY = 3;
-
-  const pets = await catAndDogFetcher.fetchAll();
-  console.log(`allPets:`, pets);
-
-  const recognizedPets = await Promise.all(
-    pets.map((pet) => magicalCatRecognizer.recognize(pet))
-  );
-
-  const cats = pets
-    .filter((pet, index) => recognizedPets[index])
-    .slice(0, TOP_QUANTITY);
-
-  return cats;
-
-  // ===
-};
-const run = async () => {
-  let topRatedCats;
-  try {
-    topRatedCats = await getCats();
-  } catch (err) {
-    console.error(err);
-  }
-
-  // Проверки
-  topRatedCats?.length !== 3 && console.error("Должно быть 3 котика");
-  !topRatedCats?.find((c) => c.id === "3") &&
-    console.error("Третий котик должен быть в топе!");
+const result = {
+    top: catsAndDogs.slice(0, 5).sort((a1, a2) => (a1.width * a1.height > a2.height * a2.width ? -1 : 1)),
+    rest: catsAndDogs.slice(5).sort((a1, a2) => (a1.width * a1.height > a2.height * a2.width ? -1 : 1)),
 };
 
-run();
+console.log(result);
+
+// Решение
+
+// Проверки
+const images = [...document.querySelectorAll("img")]
+  .map((e) => e.outerHTML.match(/data\/(.*).png*/))
+  .filter(Boolean);
+
+images.length !== 15 && console.error("На странице должно быть 15 изображений");
